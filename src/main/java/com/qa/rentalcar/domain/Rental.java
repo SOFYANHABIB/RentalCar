8d0255f8874a5357254cfa6e01e050f3ea3833cc
MEHDIHABIB <78062259+MEHDIHABIB@users.noreply.github.com>
2022-02-08 12:36:26 +0000

    Create domain package Rental class and set variables

---
+++ src/main/java/com/qa/rentalcar/domain/Rental.java
@@ -0,0 +1,27 @@
+package com.qa.rentalcar.domain;
+
+public class Rental {
+	
+	private int id;
+	
+	private int clienId;
+	
+	private int carId;
+	
+	private String clientName;
+	
+	private String carType;
+	
+	private String carBrand;
+	
+	private int rentalPeriod;
+	
+	private  boolean carBack;
+	
+	private float rentalCost;
+	
+	
+	
+	
+
+}
